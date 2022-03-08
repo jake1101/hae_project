d0b3ffa773ba47afdd4382de59ca347cbdb00883
9396341 <9396341@HAE-L9396341001.hae.global>
2022-03-08 12:25:24 +0900

    apply service controller and map

--- src/main/java/smartsuite/app/bp/serviceManage/ServiceManageService.java
+++ src/main/java/smartsuite/app/bp/serviceManage/ServiceManageService.java
@@ -146,6 +146,22 @@ public class ServiceManageService {
 		return resultMap;
 		
 	}
+	
+	/**
+	 * 서비스 신청하기. 
+	 *
+	 * @author : jake
+	 * @param param the param
+	 * @Date : 2022. 3. 8
+	 * @Method Name : applyService
+	 */ 
+	public Map<String,Object> applyService(Map param) {
+
+		sqlSession.insert("serviceManageApi.applyService", param);
+		
+		Map<String, Object> resultMap = new HashMap<String, Object>();
+		return resultMap;
+	}
 
 	/**
 	 * 메일을 전송한다. 
